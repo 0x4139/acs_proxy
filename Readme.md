fcf273a2bbf57bd70174f188c6bee4ed6f02f2b1
Vali Malinoiu <0x4139@gmail.com>
2017-03-22 21:41:25 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# What is this?
+just a proxy that get rids of the annoying stuff on www.amazingchatstories.com
+run it then open your browser on localhost:9090 and you should have a proxy that doesn't bother you as much
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net/url"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"net"
+	"golang.org/x/crypto/acme/autocert"
+	"crypto/tls"
+	"fmt"
+	"time"
+	"log"
+)
+
+func main() {
+	target := &url.URL{
+		Scheme: "http",
+		Host:   "www.amazingchatstories.com",
+	}
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.Director = func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if target.RawQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = target.RawQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = target.RawQuery + "&" + req.URL.RawQuery
+		}
+
+		req.Header.Set("Cookie", "")
+		req.Header.Set("User-Agent", fmt.Sprintf("%d", time.Now().Nanosecond()))
+
+		req.Host = target.Host
+
+	}
+
+	server := &http.Server{
+		Handler: proxy,
+		Addr:    ":9090",
+	}
+	log.Println("open your browser on http://localhost:9090")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+func redirectHttps(w http.ResponseWriter, req *http.Request) {
+	target := "https://" + req.Host + req.URL.Path
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+	http.Redirect(w, req, target,
+		http.StatusTemporaryRedirect)
+}
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func LETSENCRYPT() (*tls.Config) {
+	m := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache("./certcache"),
+	}
+	return &tls.Config{
+		GetCertificate: m.GetCertificate,
+	}
+}
